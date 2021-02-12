12e84103d4bfc1d2a6284dbde188de0f739dc2e4
Tim Rozday <48996806+timrozday@users.noreply.github.com>
2021-02-12 12:18:53 +0000

    Create __init__.py

---
+++ chembl_grounder/__init__.py
@@ -0,0 +1,5 @@
+from .chembl_grounder import ChemblGrounder
+from .chembl_name_db import ChemblNameDB
+from .chembl_name_index import ChemblNameIndex
+from .chembl_structure_index import ChemblStructureIndex
+from .gsrs_index import GsrsIndex
